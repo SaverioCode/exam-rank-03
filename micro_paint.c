5e223560ef2188c64fb2bf512e133d32a7e9bcce
Francesco Saverio Garzi Cosentino <garzicosentino@gmail.com>
2023-04-07 17:23:30 +0200

    update

--- micro_paint.c
+++ micro_paint.c
@@ -1,19 +1,6 @@
 
 #include "micro_paint.h"
 
-void	ft_free_paint(t_dflt *dflt)
-{
-	int	y;
-
-	y = 0;
-	while (dflt->paint[y])
-	{
-		free(dflt->paint[y]);
-		y++;
-	}
-	free(dflt->paint);
-}
-
 void	ft_allocate_print(t_dflt *dflt)
 {
 	int	y;
@@ -52,14 +39,13 @@ void	ft_update_paint(t_dflt *dflt, t_paint *paint)
 			else if (y >= paint->y && x >= paint->x)
 				if (y <= paint->h && x <= paint->w)
 					dflt->paint[y][x] = paint->c;
-
 			x++;
 		}
 		y++;
 	}
 }
 
-void	ft_print_paint(t_dflt *dflt)
+void	ft_print_and_free(t_dflt *dflt)
 {
 	int	y;
 
@@ -67,9 +53,11 @@ void	ft_print_paint(t_dflt *dflt)
 	while (dflt->paint[y])
 	{
 		write(1, dflt->paint[y], dflt->w);
+		free(dflt->paint[y]);
 		write(1, "\n", 1);
 		y++;
 	}
+	free(dflt->paint);
 }
 
 int	main(int ac, char **av)
@@ -110,8 +98,7 @@ int	main(int ac, char **av)
 		}
 		ft_update_paint(&dflt, &paint);
 	}
-	ft_print_paint(&dflt);
-	ft_free_paint(&dflt);
+	ft_print_and_free(&dflt);
 	return (0);
 }
 
